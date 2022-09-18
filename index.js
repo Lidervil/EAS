c4395f23ec04a48cb52c3adbbfe1c987cf34788a
ImomushiRan <george.villarroel@outlook.es>
2022-09-18 17:08:22 -0300

    Add base layout html/css

---
+++ index.js
@@ -0,0 +1,16 @@
+/*     const grid = document.getElementsByClassName('grid')[0];
+    console.log(grid)
+
+    const createGrid = (value) => {
+        gridSize = value * value;
+        for (let i = 0; i < value; i++) {
+            const div = document.createElement('div');
+            for (let j = 0; j < value; j++) {
+                const subdiv = document.createElement('div');
+                div.appendChild(subdiv);
+            }
+            grid.appendChild(div);
+        }
+    }
+
+    createGrid(16); */
